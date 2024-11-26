ee5aa2dd26a564aa317beeb3f7b32659fe6bed9e
lezajas <slezaja@veleri.hr>
2024-11-26 23:02:02 +0100

    6.edit uređivanje naslovne stranice i dodavanje datuma

--- MyPlan/src/pages/IndexPage.vue
+++ MyPlan/src/pages/IndexPage.vue
@@ -1,11 +1,24 @@
 
 <template>
   <q-page class="flex flex-center" text-h1>
+    <div class="q-pa-md q-gutter-sm">
+    <q-banner rounded class="bg-purple-8 text-h2 text-white absolute-top text-center">
+      Organiziraj svoj dan, postigni više!
+
+    </q-banner>
+  </div>
     INDEX PAGE
+    <q-banner rounded class="bg-purple-8 text-white absolute-bottom text-center">
+      {{date}}
+
+    </q-banner>
   </q-page>
+
 </template>
 
 <script setup>
+const date = new Date();
+
 defineOptions({
   name: 'IndexPage'
 });
