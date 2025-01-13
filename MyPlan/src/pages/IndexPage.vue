bf24bb36c9c11166c499da2dc8ed0be47471c7c8
lezajas <slezaja@veleri.hr>
2025-01-13 14:32:35 +0100

    7.Edit dodavanje indeks.js filea i backenda

--- MyPlan/src/pages/IndexPage.vue
+++ MyPlan/src/pages/IndexPage.vue
@@ -2,7 +2,7 @@
 <template>
   <q-page class="flex flex-center" text-h1>
     <div class="q-pa-md q-gutter-sm">
-    <q-banner rounded class="bg-purple-8 text-h2 text-white absolute-top text-center">
+    <q-banner rounded class="bg-purple-8 text-white absolute-top text-center">
       Organiziraj svoj dan, postigni više!
 
     </q-banner>
